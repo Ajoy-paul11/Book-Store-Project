1d59c1d591eab069db8af93bde9c69856797c8d1
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-08-06 12:56:05 +0530

    Add footer component and change the title of the website.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Footer />
     </>
   );
 }
