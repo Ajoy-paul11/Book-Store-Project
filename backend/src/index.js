37d77fb0b93c042060f75fc852cd0781d09d92b9
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-08-11 00:56:55 +0530

    "Updated package-lock.json and package.json to include mongoose dependency, and refactored index.js to connect to MongoDB and handle errors. Also make a file name app.js in root and take instance of the express() then export it to root index.js file."

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,29 @@
-import express from "express"
+import { app } from "./app.js"
 import dotenv from "dotenv"
+import connectDB from "./db/index.js"
 
-const app = express()
+
 
 dotenv.config({
     path: "./.env"
 })
 
-const port = process.env.PORT || 8001
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT || 8001, () => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed: ", error);
+
+    })
+
 
 app.get('/', (req, res) => {
     res.send("Hello PaulStore!")
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
 })
